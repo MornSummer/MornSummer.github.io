1c32eac26997ebc014d214710e38cc9788a77593
MornSummer <b2542225978@outlook.com>
2024-03-17 08:22:45 +0800

    Site updated: 2024-03-17 08:22:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2023/03/18/win11预览频道加入及更新方法/","2023/03/17/“第一篇文章”/","2023/03/19/“bilibiliUP主推荐”/","2023/03/26/更新日志/","2023/03/26/网站使用指南/","2023/03/26/网站意见问卷调查/"];function toRandomPost(){
+var posts=["2023/03/18/win11预览频道加入及更新方法/","2023/03/19/“bilibiliUP主推荐”/","2023/03/17/“第一篇文章”/","2023/03/26/更新日志/","2023/03/26/网站使用指南/","2023/03/26/网站意见问卷调查/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
